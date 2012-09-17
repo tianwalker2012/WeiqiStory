d71a846780f25b05e4bc0c2df4992a6ee9038117
tianwalker2012 <sunnyskyunix@gmail.com>
2012-09-17 12:10:08 +0800

    Initial Commit

---
+++ WeiqiStory/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  WeiqiStory
+//
+//  Created by xietian on 12-9-17.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
